0a6ceb8730a15625b2b9c8e4dbd4d67ae5ad85de
dldbwjd0911 <lyj040911@gmail.com>
2023-05-15 22:23:50 +0900

    202327052 이유정

--- homework01/game.c
+++ homework01/game.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "game.h"
-
 int print_title_screen() {
     // 박스 타이틀을 포함한 문자열
     char screen[] = "########################\n"
 	            "##                    ##\n"
 	            "##                    ##\n"
-		    "##   The Ballorantu   ##\n"
+		    "##   The Velorantu    ##\n"
                     "##      -Legend-      ##\n"
                     "##                    ##\n"
 		    "##                    ##\n"
@@ -14,8 +13,33 @@ int print_title_screen() {
 		    "##   2. How to play?  ##\n"
 		    "##   3. Exit          ##\n"
 		    "##                    ##\n"
+		    "##                    ##\n"
                     "########################\n";
 
-    printf("%s", screen);  // 화면에 박스 타이틀 출력
-}
+     printf("%s", screen);
 
+     return 0;
+}
+int print_how_to_play_screen() {
+
+	char screen[] = "############################"
+		        "##                        ##"
+                        "##                        ##"
+                        "##                        ##"
+			"##  Click on the monster  ##"
+			"##   on the screen with   ##"
+			"##  your mouse to defeat  ##"
+                        "##   on the screen with   ##"
+       			"##  the enemy and become  ##"
+			"##    a legendary hero!   ##"
+			"##                        ##"
+                        "##                        ##"
+			"##    .return to menu.    ##"
+                        "##                        ##"
+                        "##                        ##"
+                        "############################";
+
+   printf("%s", screen);
+
+   return 0;
+}
--- homework01/main.c
+++ homework01/main.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "game.h"
 
 int main()
